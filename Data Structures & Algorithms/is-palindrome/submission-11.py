a631df21594f7f18ecc539c25cd12e7117d9ffbf
Gavin Lim <118714957+gavinnlimm@users.noreply.github.com>
2026-08-10 11:48:17 +0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_string = ""
+
+        for char in s:
+            if char.isalnum():
+                new_string += char.lower()
+        
+        if new_string == new_string[::-1]:
+            return True
+        return False
+
